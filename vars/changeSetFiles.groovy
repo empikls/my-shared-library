ef089a84d53d32dc436f8f8d5d077fc02808f448
Yanchenko Serhii <run2cb@gmail.com>
2020-03-03 17:25:54 +0200

    edit regex

--- vars/changeSetFiles.groovy
+++ vars/changeSetFiles.groovy
@@ -3,7 +3,7 @@ def call() {
     currentBuild.changeSets.each { changeSet ->
         changeSet.items.each { entry ->
             entry.affectedFiles.each { file ->
-                if ( file.path ==~ /^prod-(ap1|eu1|us1|us2)\/\w+\-prod-(\1)\.yaml$/ ) listFilePath.add(file.path)
+                if ( file.path ==~ /^prod-(ap1|eu1|us1|us2)\/\w+.yaml$/ ) listFilePath.add(file.path)
             }
         }
     }
