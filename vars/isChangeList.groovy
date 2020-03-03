8a8ab35d67152f48720fb29f43cc908063550568
Yanchenko Serhii <run2cb@gmail.com>
2020-03-03 16:49:16 +0200

    edit isChangList

--- vars/isChangeList.groovy
+++ vars/isChangeList.groovy
@@ -1,13 +1,11 @@
-def call( name) {
-    def list = []
+def call() {
+    def listFilePath = []
     currentBuild.changeSets.each { changeSet ->
         changeSet.items.each { entry ->
             entry.affectedFiles.each { file ->
-                if (file.path ==~ /^prod-(ap1|eu1|us1|us2)\/\w+.yaml$/) {
-                    list.add(file.path)
-                }
+                if ( file.path ==~ /^prod-(ap1|eu1|us1|us2)\/\w+\-prod-(\1)\.yaml$/ ) listFilePath.add(file.path)
             }
         }
     }
-    return list.toSet()
+    return listFilePath.unique()
 }
