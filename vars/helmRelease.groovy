b572ff55675c1cc208d261965f69881c36b2f0db
Yanchenko Serhii <run2cb@gmail.com>
2020-03-06 15:00:38 +0200

    edit helmRelease

--- vars/helmRelease.groovy
+++ vars/helmRelease.groovy
@@ -1,7 +1,7 @@
 def call(String namespace, String releaseName, String valuesFile, String imageTag) {
     sh """
             echo appVersion: $imageTag >> ${imageTag}/app/Chart.yaml
-            helm upgrade --install $releaseName --namespace=$namespace --debug --force ./$imageTag/app --values ./$valuesFile  \
+            helm upgrade --install $releaseName --namespace=$namespace --debug --force $imageTag/app --values $valuesFile  \
                --set-string image.tag=$imageTag
 //            helm upgrade --dry-run --debug \
 //            --install $releaseName \
